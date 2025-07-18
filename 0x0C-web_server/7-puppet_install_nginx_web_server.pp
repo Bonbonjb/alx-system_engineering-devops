d80ff1abb90a614433d2000ac966833f4fadb47d
Brenda Bonareri <bonarerijb24@gmail.com>
2025-07-18 20:13:32 +0300

    Updated 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,44 +1,26 @@
-# 7-puppet_install_nginx_web_server.pp
+class nginx_web {
+  package { 'nginx':
+    ensure => installed,
+  }
 
-package { 'nginx':
-  ensure => installed,
+  service { 'nginx':
+    ensure     => running,
+    enable     => true,
+    require    => Package['nginx'],
+  }
+
+  file { '/var/www/html/index.nginx-debian.html':
+    ensure  => file,
+    content => "Hello World!\n",
+    require => Package['nginx'],
+  }
+
+  file { '/etc/nginx/sites-available/default':
+    ensure  => file,
+    content => template('nginx_web/default.erb'),
+    require => Package['nginx'],
+    notify  => Service['nginx'],
+  }
 }
 
-service { 'nginx':
-  ensure     => running,
-  enable     => true,
-  hasrestart => true,
-  require    => Package['nginx'],
-}
-
-file { '/var/www/html/index.html':
-  ensure  => file,
-  content => "Hello World!\n",
-  require => Package['nginx'],
-}
-
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => @(EOF),
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-
-    root /var/www/html;
-    index index.html;
-
-    server_name _;
-
-    location / {
-        try_files \$uri \$uri/ =404;
-    }
-
-    location = /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-}
-  | EOF
-  notify  => Service['nginx'],
-  require => Package['nginx'],
-}
-
+include nginx_web
