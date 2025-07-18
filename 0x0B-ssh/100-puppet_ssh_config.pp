42ec786320442a3104a6afe72e5ad24f751090e8
Brenda Jematia Bonareri <86654766+Bonbonjb@users.noreply.github.com>
2025-07-18 18:41:31 +0300

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,27 +1,34 @@
-# Ensure the SSH config file exists
+# Ensure .ssh directory exists
+file { '/home/ubuntu/.ssh':
+  ensure  => 'directory',
+  owner   => 'ubuntu',
+  group   => 'ubuntu',
+  mode    => '0700',
+}
+
+# Ensure the config file exists
 file { '/home/ubuntu/.ssh/config':
   ensure  => 'file',
   owner   => 'ubuntu',
   group   => 'ubuntu',
   mode    => '0600',
+  require => File['/home/ubuntu/.ssh'],
 }
 
 # Add IdentityFile declaration
 file_line { 'Declare identity file':
-  ensure => present,
-  path   => '/home/ubuntu/.ssh/config',
-  line   => '    IdentityFile /home/ubuntu/.ssh/school',
-  match  => '^\s*IdentityFile',
+  ensure  => present,
+  path    => '/home/ubuntu/.ssh/config',
+  line    => '    IdentityFile /home/ubuntu/.ssh/school',
+  match   => '^\s*IdentityFile',
+  require => File['/home/ubuntu/.ssh/config'],
 }
 
 # Turn off password authentication
 file_line { 'Turn off passwd auth':
-  ensure => present,
-  path   => '/home/ubuntu/.ssh/config',
-  line   => '    PasswordAuthentication no',
-  match  => '^\s*PasswordAuthentication',
+  ensure  => present,
+  path    => '/home/ubuntu/.ssh/config',
+  line    => '    PasswordAuthentication no',
+  match   => '^\s*PasswordAuthentication',
+  require => File['/home/ubuntu/.ssh/config'],
 }
-# Notice: Compiled catalog for ubuntu-xenial in environment production in 0.11 seconds
-# Notice: /Stage[main]/Main/File_line[Turn off passwd auth]/ensure: created
-# Notice: /Stage[main]/Main/File_line[Declare identity file]/ensure: created
-# Notice: Finished catalog run in 0.03 seconds
