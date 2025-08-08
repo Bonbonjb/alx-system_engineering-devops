cdcb1281634386a13a5fd38a6ffd2de71919564f
Brenda Jematia Bonareri <86654766+Bonbonjb@users.noreply.github.com>
2025-08-08 23:59:25 +0300

    Update 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,23 +1,12 @@
-# Ensure the SSH config file exists
-file { '/home/ubuntu/.ssh/config':
-  ensure  => 'file',
-  owner   => 'ubuntu',
-  group   => 'ubuntu',
-  mode    => '0600',
-}
-
-# Add IdentityFile declaration
-file_line { 'Declare identity file':
-  ensure => present,
-  path   => '/home/ubuntu/.ssh/config',
-  line   => '    IdentityFile /home/ubuntu/.ssh/school',
-  match  => '^\s*IdentityFile',
-}
-
-# Turn off password authentication
+# Puppet script to create ssh config file
 file_line { 'Turn off passwd auth':
-  ensure => present,
-  path   => '/home/ubuntu/.ssh/config',
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
-  match  => '^\s*PasswordAuthentication',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
 }
